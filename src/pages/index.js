8afa91fc8c0228f4acc8e89b7bdc74f3db75e59d
Andrew Ngo <andrew@andrews-mbp-3.lan>
2018-10-07 18:33:44 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi everyone!</h1>
+    <p>My name is Andrew Ngo!</p>
+    <p>I am a recent graduate from Hack Reactor and experienced in full stack with React and MySQL</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage

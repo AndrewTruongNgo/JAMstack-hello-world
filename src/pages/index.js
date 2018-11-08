a6ec39c7bc229128d39b63e0e1bfce0d05d51ee1
Andrew Ngo <andrew@Andrews-MacBook-Pro-3.local>
2018-11-07 19:39:59 -0800

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ const IndexPage = () => (
     <h1>Hi everyone!</h1>
     <p>My name is Andrew Ngo!</p>
     <p>I am a recent graduate from Hack Reactor and experienced in full stack with React and MySQL</p>
+    <p>test</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
